6fae5feb57a4314567b4b4844148772fed96ba6b
Trần Ngọc Phát <tranngocphat240111@gmail.com>
2025-11-23 12:43:03 +0700

    feat: initial source setup

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-50">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
