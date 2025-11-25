a29165ae688e6f4baa9588b5989f30305619e092
Trần Ngọc Phát <tranngocphat240111@gmail.com>
2025-11-25 13:14:21 +0700

    fix CORS

--- server/src/main/java/iuh/fit/server/controller/OrderController.java
+++ server/src/main/java/iuh/fit/server/controller/OrderController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 @Slf4j
-@CrossOrigin(origins = "*")
+// CORS is configured globally in WebConfig.java
 public class OrderController {
 
     private final OrderService orderService;
